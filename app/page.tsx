b63ad98c8d36d334cf165d1fa922a3310ee9793e
harsh <hanzo4679@gmail.com>
2025-09-06 17:10:39 +0000

    add theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
+"use client";
 import { PlayerSelector } from "./PlayerSelector";
 import type { PlayerSelectorProps } from "./PlayerSelector";
+import { useTheme } from "next-themes";
 
 export default function Page() {
+  const { theme, setTheme } = useTheme();
   return (
     <div className="flex flex-col h-screen justify-center items-center">
+
+      <button 
+      className="absolute top-0 right-0 p-4 m-2
+      text-4xl ">
+        {theme === "dark" ? "🌙" : "🌞"}
+      </button>
+
       <PlayerSelector
-      unclickedPlaceholder="Get stats on your favourite player"
-      notFoundPlaceholder="No player found"
-      clickedPlaceholder="search by name"
+        unclickedPlaceholder="Get stats on your favourite player"
+        notFoundPlaceholder="No player found"
+        clickedPlaceholder="search by name"
         players={[
           {
             label: "Virat Kohli",
@@ -24,7 +34,7 @@ export default function Page() {
         ]}
       />
       <div className="absolute bottom-0 my-1 font-mono text-xs text-neutral-700">
-      Made by Harsh Tandekar CH23B084
+        Made by Harsh Tandekar CH23B084
       </div>
     </div>
   )
